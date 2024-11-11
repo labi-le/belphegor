6045cfcf46995a2e6271ea159e87b15bc1e96a0b
labi-le <65619560+labi-le@users.noreply.github.com>
2024-11-11 03:24:44 +0000

    chore: update flake.nix and flake.lock to v1.5.1

--- flake.nix
+++ flake.nix
@@ -8,18 +8,18 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     let
-      version = "1.2.0";
+      version = "1.5.1";
       pname = "belphegor";
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
 
       systemConfigs = {
         x86_64-linux = {
           arch = "linux_amd64";
-          hash = "sha256-tWdhdNRrGFnuzeNzUwZ3xgmUfTIbsE+9LESFfy1zR9o="; # x86_64-linux
+          hash = "sha256-zmbuCx/xFIgGBOR5U2ImJPEkhXDYe4qRXdgqcX3RJsI="; # x86_64-linux
         };
         aarch64-linux = {
           arch = "linux_armv6";
-          hash = "sha256-Vx6a4ksVicZOI6PppubFgo/Gym6u5UuEtL8bh9diq5A="; # aarch64-linux
+          hash = "sha256-F3eUS4gLG8sXL5llfJyrYTRPIZU+g8hDN9jxG9+rgJY="; # aarch64-linux
         };
       };
     in
