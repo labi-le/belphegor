9eb99a58f6309ebce22fdaa2ce510fcc75cb3cde
labi-le <1a6i1e@gmail.com>
2024-11-22 12:51:18 +0300

    remove mutex

--- internal/node/last.go
+++ internal/node/last.go
@@ -2,13 +2,11 @@ package node
 
 import (
 	"github.com/labi-le/belphegor/internal/types/domain"
-	"sync"
 )
 
 // LastMessage which is stored in Node and serves to identify duplicate messages
 type LastMessage struct {
 	*domain.Message
-	mu     sync.Mutex
 	Update chan *domain.Message
 }
 
@@ -16,18 +14,9 @@ func NewLastMessage() *LastMessage {
 	return &LastMessage{Update: make(chan *domain.Message)}
 }
 
-func (m *LastMessage) Get() *domain.Message {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-
-	return m.Message
-}
-
 // ListenUpdates updates the lastMessage with the latest message received
 func (m *LastMessage) ListenUpdates() {
 	for msg := range m.Update {
-		m.mu.Lock()
 		m.Message = msg
-		m.mu.Unlock()
 	}
 }
