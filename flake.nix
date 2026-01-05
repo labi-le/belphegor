ec7d90e913d53e7c961a33d3232c954796c28707
labi-le <65619560+labi-le@users.noreply.github.com>
2026-01-05 11:10:16 +0000

    chore: update flake.nix and flake.lock to v3.1.0

--- flake.nix
+++ flake.nix
@@ -8,18 +8,18 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     let
-      version = "3.0.1";
+      version = "3.1.0";
       pname = "belphegor";
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
 
       systemConfigs = {
         x86_64-linux = {
           arch = "linux_amd64";
-          hash = "sha256-mmwHV+jYqK5NVpdgBguIzQEcsvfHBgyGid83UPR6LwE="; # x86_64-linux
+          hash = "sha256-QvhF1FjLRSM5hlFsuAKQnK+dJHSb+KXybn629lkEKrI="; # x86_64-linux
         };
         aarch64-linux = {
           arch = "linux_armv6";
-          hash = "sha256-JxiVnaY8eHdEUTVsmE0kcAU73H1Ab5mAtJJFWE4N73Q="; # aarch64-linux
+          hash = "sha256-C88EBZ5tLvhCvXk664i7PJ3E22Tsq3I650nUCzhu5v4="; # aarch64-linux
         };
       };
     in
